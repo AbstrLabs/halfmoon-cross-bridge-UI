ad367d6c306cae35ae4c392cca71ed31efb76aac
Pablion <36828324+Pablion@users.noreply.github.com>
2022-05-20 23:44:51 +0200

    feat: fix E404 page style

--- src/page/E404Page.tsx
+++ src/page/E404Page.tsx
@@ -4,10 +4,11 @@ import { Typography } from "@mui/material";
 export function E404Page() {
   return (
     <Box textAlign="center">
-      <Typography variant="h1">not found</Typography>
-      <Typography variant="body1">Route not found</Typography>
+      <Box height="20px" />
+      <Typography variant="h2">NOT FOUND</Typography>
+      <Typography variant="body1">page not found</Typography>
       <img
-        style={{ maxWidth: "80%" }}
+        style={{ maxWidth: "60vw", maxHeight: "60vh" }}
         alt="page-not-found"
         src={process.env.PUBLIC_URL + "/svg/page-not-found.svg"}
         title="temp pic from https://all-free-download.com/free-vector/download/error-404-page-not-found_6845510.html"
