8e8b0bc53434128ddc070f43ec1e5b5bcc2f8fa1
Pablion <36828324+Pablion@users.noreply.github.com>
2022-05-21 12:51:24 +0200

    feat: add a result page

--- src/page/Router.tsx
+++ src/page/Router.tsx
@@ -5,6 +5,7 @@ import { E404Page } from "./E404Page";
 import { Header } from "../component/Header";
 import { HomePage } from "./HomePage";
 import { ProcessPage } from "./ProcessPage";
+import { ResultPage } from "./ResultPage";
 
 export function Router() {
   /* ======== URL_QUERY ======== */
@@ -35,7 +36,7 @@ export function Router() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/process" element={<ProcessPage />} />
-        <Route path="/result" element={<HomePage />} />
+        <Route path="/result" element={<ResultPage />} />
         <Route path="/help" element={<HomePage />} />
         <Route path="*" element={<E404Page />} />
       </Routes>
