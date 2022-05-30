AbstrLabs/halfmoon-cross-bridge-UI

commit c7cc349ec0d05f7a75872bb17dda7f1b3d6ef00b

fix: TSteps interface
`type TSteps` stores general information about how the steps are defined
Page state is stored in variable `isStepsFinished` for performance
Otherwise, the whole `steps` variable will be updated on each render

With this idea, status should not be here(next commit).
`finished` attr is more like actionOnFinished and is not used.
The order of the steps is controlled by the attr `stepId`.
`stepId` is not perfect, but we use it to use the interface of `Stepper`

src/context/panel.tsx
@@ -6,15 +6,10 @@ import { TxnType } from "../js/config";
 import algosdk from "algosdk";
 import { useCountdown } from "usehooks-ts";
 
-type TTxnSteptype = string | number | symbol;
-
 enum TTxnStepName {
-  WALLET_START = "Connect to Wallet",
   WALLET_CONNECTED = "Connected",
   FORM_START = "Fill up the Form",
-  FORM_FILLED = "Filled",
   AUTH = "Authorize the Transaction",
-  AUTH_START = "Start the Transaction",
 }
 
 const DEFAULT_MINT_BENEFICIARY =
@@ -34,11 +29,10 @@ type TStep = {
   icon: JSX.Element;
   action: (() => void) | (() => Promise<any>);
   status: boolean;
-  finished: string;
 };
 
 type TSteps = {
-  [key in TTxnSteptype]: TStep;
+  [key in TTxnStepName]: TStep;
 };
 
 // create context for panel
@@ -237,12 +231,11 @@ const PanelContextProvider = ({
   // steps
   const steps: TSteps = useMemo(
     () => ({
-      [TTxnStepName.WALLET_START]: {
+      [TTxnStepName.WALLET_CONNECTED]: {
         stepId: 0,
         icon: <></>,
         action: async () => await connectWallet(),
         status: connectedAcc.length > 0 ? true : false,
-        finished: TTxnStepName.WALLET_CONNECTED + connectedAcc,
       },
 
       [TTxnStepName.FORM_START]: {
@@ -254,7 +247,6 @@ const PanelContextProvider = ({
           beneficiary.length > 0 &&
           isAmountValid &&
           isBeneficiaryValid,
-        finished: TTxnStepName.FORM_FILLED,
       },
 
       [TTxnStepName.AUTH]: {
@@ -267,7 +259,6 @@ const PanelContextProvider = ({
           beneficiary.length > 0 &&
           isAmountValid &&
           isBeneficiaryValid,
-        finished: TTxnStepName.AUTH_START,
       },
     }),
     [

commit 07eedba207c505a85657747e49c5881a05fd852b

fix: fix broken button title broken by "c7cc349".

src/component/TxnPanel.tsx
@@ -109,7 +109,7 @@ export function TxnPanel() {
                   color="inherit"
                   onClick={stepObject.action}
                   disabled={
-                    stepObject.stepId != 2
+                    stepObject.stepId !== 2
                       ? panel.isStepsFinished
                         ? stepObject.stepId >
                           panel.isStepsFinished.findIndex((x) => !x)
@@ -117,10 +117,10 @@ export function TxnPanel() {
                       : !stepObject.status
                   }
                 >
-                  {panel.isStepsFinished?.[stepObject.stepId] &&
-                  stepObject.status
-                    ? stepObject.finished
-                    : stepName}
+                  {/* button title */}
+                  {panel.isStepsFinished[stepObject.stepId]
+                    ? stepObject.title.finished
+                    : stepObject.title.default}
                 </StepButton>
               </Step>
             ))

src/context/panel.tsx
@@ -29,6 +29,11 @@ type TStep = {
   icon: JSX.Element;
   action: (() => void) | (() => Promise<any>);
   status: boolean;
+  title: {
+    default: string;
+    finished: string;
+    // can add a error message here
+  };
 };
 
 type TSteps = {
@@ -87,7 +92,7 @@ const PanelContextProvider = ({
   // connect and step function
   //
   const [nearAcc, setNearAcc] = useState<string>(
-    nearWallet.account().accountId
+    nearWallet.account().accountId ?? ""
   );
   const [algoAcc, setAlgoAcc] = useState<string>("");
   const connectedAcc = useMemo(
@@ -228,6 +233,8 @@ const PanelContextProvider = ({
     ]
   );
 
+  console.log("connectedAcc.length : ", connectedAcc.length); // DEV_LOG_TO_REMOVE
+
   // steps
   const steps: TSteps = useMemo(
     () => ({
@@ -236,8 +243,11 @@ const PanelContextProvider = ({
         icon: <></>,
         action: async () => await connectWallet(),
         status: connectedAcc.length > 0 ? true : false,
+        title: {
+          default: "Connect to Wallet",
+          finished: "Wallet Connected",
+        },
       },
-
       [TxnStepName.VALIDATE_FORM]: {
         stepId: 1,
         icon: <></>,
@@ -247,6 +257,10 @@ const PanelContextProvider = ({
           beneficiary.length > 0 &&
           isAmountValid &&
           isBeneficiaryValid,
+        title: {
+          default: "Fill up the Form",
+          finished: "Form Validated",
+        },
       },
 
       [TxnStepName.AUTH_TXN]: {
@@ -259,6 +273,10 @@ const PanelContextProvider = ({
           beneficiary.length > 0 &&
           isAmountValid &&
           isBeneficiaryValid,
+        title: {
+          default: "Start the Transaction",
+          finished: "Transaction Authorized",
+        },
       },
     }),
     [