4e457cfca8e3cc74fea3f1e71aa150f06d07f106
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-18 18:39:09 +0200

    ref: ren "api-call" -> "call-server"

--- src/api-deps/call-server.ts
+++ src/api-deps/call-server.ts
@@ -1,6 +1,12 @@
 import { ApiCallParam } from "../util/shared-types/api";
 import { CONFIG } from "./config";
 
+/**
+ * call server with POST method
+ *
+ * @param postParam
+ * @returns
+ */
 export async function postTxn(postParam: ApiCallParam) {
   // todo: refactor this type after backend refactor
 
@@ -17,6 +23,12 @@ export async function postTxn(postParam: ApiCallParam) {
   return resp; // will have a {uid, BridgeTxnStatus} json in body
 }
 
+/**
+ * call server with POST method
+ *
+ * @param uid - uid of the txn with format {DbId}.{TxnId}
+ * @returns
+ */
 export async function getTxn(uid: string) {
   const resp = await fetch(`${CONFIG.apiServerUrl}/?uid=${uid}`);
   return resp; // will have a BridgeTxnObj in body
